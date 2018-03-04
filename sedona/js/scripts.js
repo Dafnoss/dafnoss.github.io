af7995fbec8998780f082d1fa64eec8134de3451
Daffna <komkov-1@ya.ru>
2018-03-05 01:49:24 +0700

    обновил седону

--- sedona/js/scripts.js
+++ sedona/js/scripts.js
@@ -1,1 +1,20 @@
-var test = 0;
+
+function scriptsRun () {
+
+    function getButton () {
+        var button = document.querySelector(".btn");
+
+        button.onclick = function () {
+            var searchBlock = document.querySelector('.search-form');
+            if (searchBlock.classList.contains('hide')) {
+                searchBlock.classList.remove('hide')
+            } else
+            searchBlock.classList.add('hide')
+        }
+    };
+
+    getButton();
+
+}
+
+window.onload = scriptsRun;
